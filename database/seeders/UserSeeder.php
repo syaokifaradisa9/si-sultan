44d57ed6b8d97bd98a768ace10e6e524647a595b
ihsanfansyuri <67683530+ihsanfansyuri@users.noreply.github.com>
2022-09-15 09:47:18 +0800

    Menambahkan admin user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,5 +15,12 @@ class UserSeeder extends Seeder
     public function run()
     {
         User::factory(3)->create();
+
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('password'),
+            'role' => 'superadmin'
+        ]);
     }
 }
