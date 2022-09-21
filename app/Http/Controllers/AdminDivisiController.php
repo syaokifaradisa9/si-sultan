802e6da5369d45f167ed0179efc9bf0e2da6f947
melky_hs <melkyherman29@gmail.com>
2022-09-21 15:21:26 +0800

    Menambahkan tabel inventory tidak habis pakai dan habis pakai pada view admin divisi

--- app/Http/Controllers/AdminDivisiController.php
+++ app/Http/Controllers/AdminDivisiController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Inventory;
+use App\Models\InventoryHp;
 
 class AdminDivisiController extends Controller
 {
     public function index()
     {
-        return view('roles.admin.index');
+        $inventory = Inventory::all();
+        $inventory_hp = InventoryHp::all();
+        return view('roles.admin.index', ['inventories' => $inventory, 'inventory_hps' => $inventory_hp, 'title' => 'Inventory Barang', 'header' => 'Inventory Barang']);
     }
 }
--- resources/views/roles/admin/index.blade.php
+++ resources/views/roles/admin/index.blade.php
@@ -1,5 +1,101 @@
 @extends('layouts.main')
 
 @section('container')
-  Ini adalah halaman admin bagian
+<div class="card">
+            <div class="card-header">
+              <h4>Barang Tidak Habis Pakai</h4>
+              <div class="card-header-form">
+                <form>
+                </form>
+              </div>
+            </div>
+            <div class="card-body p-0">
+              <div class="table-responsive">
+                <table class="table table-bordered" id="order-table">
+                  <thead>
+                    <tr>
+                      <th class="text-center" style="width: 15px">No</th>
+                      <th class="text-center" style="width: 220px">Nama Barang</th>
+                      <th class="text-center" style="width: 15px">Baik</th>
+                      <th class="text-center" style="width: 15px">Rusak Ringan</th>
+                      <th class="text-center" style="width: 15px">Rusak Berat</th>
+                      <th class="text-center" style="width: 300px">Spesifikasi</th>
+                      <th class="text-center" style="width: 300px">Justifikasi</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    @foreach ($inventories as $index => $inventory)
+                      <tr>
+                        <td class="text-center align-middle">
+                        {{ $index + 1 }}
+                        </td>
+                        <td>
+                        {{ $inventory -> nama_barang }}
+                        </td>
+                        <td class="text-center align-middle">
+                        {{ $inventory -> baik }}
+                        </td>
+                        <td class="text-center align-middle">
+                        {{ $inventory -> rusak_ringan }}
+                        </td>
+                        <td class="text-center align-middle">
+                        {{ $inventory -> rusak_berat }}
+                        </td>
+                        <td class="text-justify align-middle">
+                          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam neque deserunt inventore nobis quasi laborum molestiae officiis a hic, perspiciatis voluptate, voluptatibus dolorem unde reprehenderit et cupiditate. Obcaecati, inventore asperiores.
+                        </td>
+                        <td class="text-justify align-middle">
+                         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam quibusdam aliquid, ut temporibus fuga dolorum aperiam suscipit expedita, perferendis impedit nisi dolores, at et tempore culpa adipisci magni fugit! Laboriosam?
+                        </td>
+
+                      </tr>
+                      @endforeach
+                  </tbody>
+                </table>
+              </div>
+            </div>
+</div>
+<div class="card">
+            <div class="card-header">
+              <h4>Barang Habis Pakai</h4>
+              <div class="card-header-form">
+                <form>
+                </form>
+              </div>
+            </div>
+            <div class="card-body p-0">
+              <div class="table-responsive">
+                <table class="table table-bordered" id="order-table">
+                  <thead>
+                    <tr>
+                      <th class="text-center" style="width: 15px">No</th>
+                      <th class="text-center" style="width: 220px">Nama Barang</th>
+                      <th class="text-center" style="width: 15px">Total</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    @foreach ($inventory_hps as $index => $inventory_hp)
+                      <tr>
+                        <td class="text-center align-middle">
+                        {{ $index + 1 }}
+                        </td>
+                        <td class="text-center align-middle">
+                        {{ $inventory_hp -> nama_barang }}
+                        </td>
+                        <td class="text-center align-middle">
+                        {{ $inventory_hp -> total }}
+                        </td>
+                      </tr>
+                      @endforeach
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+        </div>
+      </div>
+      
+    </div>
 @endsection
