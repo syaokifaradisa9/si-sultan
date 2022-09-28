0d11e79bb970a81c4c624c44e6d8986e474fe730
ihsanfansyuri <67683530+ihsanfansyuri@users.noreply.github.com>
2022-09-28 16:25:19 +0800

    Memperbaiki routes tata_operasional

--- app/Http/Controllers/To/TataOperasionalController.php
+++ app/Http/Controllers/To/TataOperasionalController.php
@@ -7,14 +7,14 @@ use Illuminate\Http\Request;
 
 class TataOperasionalController extends Controller
 {
-    public function index()
-    {
-        return view('roles.tata_operasionl.index');
-    }
-    public function order()
-    {
-      return view('roles.to.order', [
-        'header' => 'Order'
-      ]);
-    }
+  public function index()
+  {
+    return view('roles.tata_operasionl.index');
+  }
+  public function order()
+  {
+    return view('roles.tata_operasional.order', [
+      'header' => 'Order'
+    ]);
+  }
 }
