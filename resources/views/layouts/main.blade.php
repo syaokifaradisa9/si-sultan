3f09f0e73d120658a66551c9a535e465b14fce0b
AceTaker <melkyherman29@gmail.com>
2022-10-17 12:11:33 +0800

    Mengupdate tampilan semua user

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -2,89 +2,93 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
-  <meta name="csrf-token" content="{{ csrf_token() }}">
-  <title>{{ $title ?? 'SI-SULTAN' }}</title>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>{{ $title ?? 'SI-SULTAN' }}</title>
 
-  <!-- General CSS Files -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.7.2/css/all.min.css"
-    integrity="sha512-3M00D/rn8n+2ZVXBO9Hib0GKNpkm8MSUU/e2VNthDyBYxKWG+BftNYYcuEjXlyrSO637tidzMBXfE7sQm0INUg==" crossorigin="anonymous"
-    referrerpolicy="no-referrer" />
-  <link href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css" rel="stylesheet">
-  <link href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap.min.css" rel="stylesheet">
+    <!-- General CSS Files -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.7.2/css/all.min.css"
+        integrity="sha512-3M00D/rn8n+2ZVXBO9Hib0GKNpkm8MSUU/e2VNthDyBYxKWG+BftNYYcuEjXlyrSO637tidzMBXfE7sQm0INUg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css" rel="stylesheet">
+    <link href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap.min.css" rel="stylesheet">
 
-  <!-- CSS Libraries -->
+    <!-- CSS Libraries -->
 
-  {{-- script jquery dan datatabel --}}
-  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>
-  <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-  <script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap5.min.js"></script>
+    {{-- script jquery dan datatabel --}}
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>
+    <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    <script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap5.min.js"></script>
 
-  <!-- Template CSS -->
-  <link rel="stylesheet" href="{{ asset('vendor/stisla/css/style.css') }}">
-  <link rel="stylesheet" href="{{ asset('vendor/stisla/css/components.css') }}">
+
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="{{ asset('vendor/stisla/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/stisla/css/components.css') }}">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-social/5.1.1/bootstrap-social.css"></script>
 </head>
 
 <body>
-  <div id="app">
-    <div class="main-wrapper">
-      <div class="navbar-bg"></div>
+    <div id="app">
+        <div class="main-wrapper">
+            <div class="navbar-bg"></div>
 
-      {{-- navbar --}}
-      @include('partials.navbar')
+            {{-- navbar --}}
+            @include('partials.navbar')
 
-      {{-- sidebar --}}
-      @include('partials.sidebar')
+            {{-- sidebar --}}
+            @include('partials.sidebar')
 
-      <!-- Main Content -->
-      <div class="main-content">
-        <section class="section">
-          <div class="section-header">
-            <h1>{{ $header ?? 'Blank Page' }}</h1>
-          </div>
+            <!-- Main Content -->
+            <div class="main-content">
+                <section class="section">
+                    <div class="section-header">
+                        <h1>{{ $header ?? 'Blank Page' }}</h1>
+                    </div>
 
-          <div class="section-body">
-            @yield('container')
-          </div>
-        </section>
-      </div>
-      <footer class="main-footer">
-        <div class="footer-left">
-          Copyright &copy; 2022 - Depeloved By Siapa, ya?
+                    <div class="section-body">
+                        @yield('container')
+                    </div>
+                </section>
+            </div>
+            <footer class="main-footer">
+                <div class="footer-left">
+                    Copyright &copy; 2022 - Depeloved By Siapa, ya?
+                </div>
+                <div class="footer-right">
+
+                </div>
+            </footer>
         </div>
-        <div class="footer-right">
-
-        </div>
-      </footer>
     </div>
-  </div>
 
-  <!-- General JS Scripts -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-    integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-    integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.nicescroll/3.7.6/jquery.nicescroll.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
-  <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-  <script src="{{ asset('vendor/stisla/js/stisla.js') }}"></script>
+    <!-- General JS Scripts -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.nicescroll/3.7.6/jquery.nicescroll.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="{{ asset('vendor/stisla/js/stisla.js') }}"></script>
 
-  <!-- JS Libraies -->
+    <!-- JS Libraies -->
 
-  <!-- Template JS File -->
-  <script src="{{ asset('vendor/stisla/js/scripts.js') }}"></script>
-  <script src="{{ asset('assets/js/alert.js') }}"></script>
-  <script src="{{ asset('assets/js/btn-function.js') }}"></script>
-  <script src="{{ asset('assets/js/datatable.js') }}"></script>
-  <script src="{{ asset('assets/js/form-validate.js') }}"></script>
-  <script src="{{ asset('assets/js/usulan-script.js') }}"></script>
+    <!-- Template JS File -->
+    <script src="{{ asset('vendor/stisla/js/scripts.js') }}"></script>
+    <script src="{{ asset('assets/js/alert.js') }}"></script>
+    <script src="{{ asset('assets/js/btn-function.js') }}"></script>
+    <script src="{{ asset('assets/js/datatable.js') }}"></script>
+    <script src="{{ asset('assets/js/form-validate.js') }}"></script>
+    <script src="{{ asset('assets/js/usulan-script.js') }}"></script>
 
-  <!-- Page Specific JS File -->
+    <!-- Page Specific JS File -->
 </body>
 
 </html>
--- resources/views/roles/admin/index.blade.php
+++ resources/views/roles/admin/index.blade.php
@@ -1,47 +1,51 @@
 @extends('layouts.main')
 
 @section('container')
-  <div class="card">
-    <div class="card-header">
-      <h4>Barang Habis Pakai</h4>
-    </div>
-    <div class="card-body">
-      <div class="table-responsive">
-        <table class="table table-bordered" id="used-table">
-          <thead>
-            <tr>
-              <th class="text-center" style="width: 15px">No</th>
-              <th class="text-center" style="width: 220px">Nama Barang</th>
-              <th class="text-center" style="width: 15px">Total</th>
-            </tr>
-          </thead>
-          <tbody>
-          </tbody>
-        </table>
-      </div>
-    </div>
-  </div>
-
-  <div class="card">
-    <div class="card-header">
-      <h4>Barang Tidak Habis Pakai</h4>
-    </div>
-    <div class="card-body">
-      <div class="table-responsive">
-        <table class="table table-bordered" id="order-table">
-          <thead>
-            <tr>
-              <th class="text-center" style="width: 15px">No</th>
-              <th class="text-center">Nama Barang</th>
-              <th class="text-center" style="width: 200px">Baik</th>
-              <th class="text-center" style="width: 200px">Rusak Ringan</th>
-              <th class="text-center" style="width: 200px">Rusak Berat</th>
-            </tr>
-          </thead>
-          <tbody>
-          </tbody>
-        </table>
-      </div>
-    </div>
-  </div>
-@endsection
+    <div class="row">
+        <div class="col-12 col-md-6 col-lg-6">
+            <div class="card">
+                <div class="card-header">
+                    <h4>Barang Habis Pakai</h4>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-bordered table-md" id="used-table">
+                            <thead>
+                                <tr>
+                                    <th class="text-center">No</th>
+                                    <th class="text-center">Nama Barang</th>
+                                    <th class="text-center">Total</th>
+                                </tr>
+                            </thead>
+                            <tbody class="text-center">
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-md-6 col-lg-6">
+            <div class="card">
+                <div class="card-header">
+                    <h4>Barang Tidak Habis Pakai</h4>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-bordered table-md" id="order-table">
+                            <thead>
+                                <tr>
+                                    <th class="text-center">No</th>
+                                    <th class="text-center">Nama Barang</th>
+                                    <th class="text-center">Baik</th>
+                                    <th class="text-center">Rusak Ringan</th>
+                                    <th class="text-center">Rusak Berat</th>
+                                </tr>
+                            </thead>
+                            <tbody class="text-center">
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endsection
